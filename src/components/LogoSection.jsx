1aad6feed1f1762dace4e460cf71403e20fac368
TVPhuc <tvphuc2k4@gmail.com>
2025-03-14 16:30:18 +0700

    Update Logo

--- src/components/LogoSection.jsx
+++ src/components/LogoSection.jsx
@@ -2,7 +2,7 @@ export default function LogoSection() {
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center">
-        <img src="../../public/OIP.jpg" alt="Logo" className="w-12" />
+        <img src="/public/OIP.jpg" alt="Logo" className="w-12" />
         <h2 className=" text-red-400 font-bold mt-2">SAIGON CO.OP</h2>
       </div>
       <h2 className=" font-bold mt-2">Phần Mềm In Tem Dán</h2>
