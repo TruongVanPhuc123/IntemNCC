909637bf073cddb2cfea1aea2abdfb37ecf76a60
Kho - TTPP - D2 <khod2@ttpp2.com>
2025-04-08 08:03:33 +0700

    update-landing

--- src/components/landing/LogoSection.jsx
+++ src/components/landing/LogoSection.jsx
@@ -1,10 +1,10 @@
 export default function LogoSection() {
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex items-center">
+    <div className="flex justify-center items-center">
+      {/* <div className="flex items-center">
         <img src="/OIP.jpg" alt="Logo" className="w-12" />
         <h2 className=" text-red-600 font-bold mt-2">SAIGON CO.OP</h2>
-      </div>
+      </div> */}
       <h2 className=" font-bold mt-2">Phần Mềm Tạo Tem Dán</h2>
     </div>
   );
